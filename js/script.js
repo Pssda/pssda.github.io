0bfec3876f107400f6268e384246c5f045833e2c
Jason Park <parkjs814@gmail.com>
2017-06-08 19:30:09 +0900

    Added about and contact pages && Made it responsive

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(() => {
   }, 2000);
 
   const onResize = () => {
-    const half_champagne_right = $('.page-consulting .half-champagne-right');
-    const half_champagne_left = $('.page-consulting .half-champagne-left');
+    const half_champagne_right = $('.half-champagne-right');
+    const half_champagne_left = $('.half-champagne-left');
     const { clientWidth: width, clientHeight: height } = document.body;
     const hyp = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
     const angle = Math.atan(width / 2 / height) / Math.PI * 180;
@@ -26,9 +26,47 @@ $(document).ready(() => {
   $(window).scroll(function () {
     const offset = $(window).scrollTop();
     $('.nav').toggleClass('visible', offset > 57);
+    $('.champagne').css('background-position-y', 1024 + offset / 4);
+    $('.half-champagne-right').css('background-position-y', -offset / 4);
+    $('.half-champagne-left').css('background-position-y', offset / 4);
   });
 
   $('#menu').change(function () {
     $('.panel-sidebar').toggleClass('visible', this.checked);
+  });
+
+  $(document).on('click', 'a', function (e) {
+    const selector = $.attr(this, 'href');
+    if (!selector.startsWith('#')) return;
+
+    e.preventDefault();
+    $('body').animate({
+      scrollTop: $(selector).offset().top
+    }, 500);
+    $('#menu').prop('checked', false);
+    $('#menu').change();
+  });
+
+  const filter = [
+    [$('.section-eye-left .eye'), 'graphic'],
+    [$('.section-eye-right .eye'), 'consulting'],
+    [$('.band'), 'web'],
+  ];
+
+  filter.forEach(([$el, key]) => {
+    $el.click(() => {
+      $('.category').addClass('hide');
+      $(`.category-${key}`).removeClass('hide');
+      $('body').animate({
+        scrollTop: $('#portfolio').offset().top
+      }, 500);
+    });
+  });
+
+  $('.nav svg').click(() => {
+    $('.category').removeClass('hide');
+    $('body').animate({
+      scrollTop: 0
+    }, 500);
   });
 });
--- script.js
+++ script.js
@@ -7,6 +7,8 @@
  */
 'use strict';
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 $(document).ready(function () {
   window.setTimeout(function () {
     $('body').removeClass('loading');
@@ -14,8 +16,8 @@ $(document).ready(function () {
   }, 2000);
 
   var onResize = function onResize() {
-    var half_champagne_right = $('.page-consulting .half-champagne-right');
-    var half_champagne_left = $('.page-consulting .half-champagne-left');
+    var half_champagne_right = $('.half-champagne-right');
+    var half_champagne_left = $('.half-champagne-left');
     var _document$body = document.body,
         width = _document$body.clientWidth,
         height = _document$body.clientHeight;
@@ -38,9 +40,47 @@ $(document).ready(function () {
   $(window).scroll(function () {
     var offset = $(window).scrollTop();
     $('.nav').toggleClass('visible', offset > 57);
+    $('.champagne').css('background-position-y', 1024 + offset / 4);
+    $('.half-champagne-right').css('background-position-y', -offset / 4);
+    $('.half-champagne-left').css('background-position-y', offset / 4);
   });
 
   $('#menu').change(function () {
     $('.panel-sidebar').toggleClass('visible', this.checked);
+  });
+
+  $(document).on('click', 'a', function (e) {
+    var selector = $.attr(this, 'href');
+    if (!selector.startsWith('#')) return;
+
+    e.preventDefault();
+    $('body').animate({
+      scrollTop: $(selector).offset().top
+    }, 500);
+    $('#menu').prop('checked', false);
+    $('#menu').change();
+  });
+
+  var filter = [[$('.section-eye-left .eye'), 'graphic'], [$('.section-eye-right .eye'), 'consulting'], [$('.band'), 'web']];
+
+  filter.forEach(function (_ref) {
+    var _ref2 = _slicedToArray(_ref, 2),
+        $el = _ref2[0],
+        key = _ref2[1];
+
+    $el.click(function () {
+      $('.category').addClass('hide');
+      $('.category-' + key).removeClass('hide');
+      $('body').animate({
+        scrollTop: $('#portfolio').offset().top
+      }, 500);
+    });
+  });
+
+  $('.nav svg').click(function () {
+    $('.category').removeClass('hide');
+    $('body').animate({
+      scrollTop: 0
+    }, 500);
   });
 });
